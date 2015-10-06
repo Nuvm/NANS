b5631dfd5c9170d9ab737eb8c28db88e18306652
Nuvm <nuvmyt@gmail.com>
2015-10-06 15:34:31 -0400

    Delete CustomCSS.js

--- CustomCSS.js
+++
@@ -1,8 +0,0 @@
-var checkIfReady;
-setTimeout(function(){checkIfReady = setInterval(function(){if(document.getElementsByClassName('loading').length!==1){start('ready');}},100);},2000);
-function start(s){
-  clearInterval(checkIfReady);
-  if(s==='ready'){
-    $('head').append('<link rel="stylesheet" href="https://cdn.rawgit.com/Nuvm/NCS/master/NC331_Style.css">');
-  }
-}
