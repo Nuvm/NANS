11ea88210d31ed101f870952498cc554050f1639
CSxKING <bentenz5@live.com>
2015-10-06 19:29:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var version = '0.1.1 Minor 3';
 var startUpMsg = 'Welcome to NCS version ' + version + '!';
-var newFeaturesMsg = 'The NCS Website is now online! https://electricgaming.ga/ncs/ <b><i>BIG CHANGES SOON</b></i>(™) :)';
+var newFeaturesMsg = 'The NCS Website is now online! <a href="https://electricgaming.ga/ncs/" target="_blank">Click here to go to it!</a> <b><i>BIG CHANGES SOON</b></i>(™) :)';
 var uname;
 
 //Default vars
