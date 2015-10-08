7f139f67158381bacce236eacee1ab566c5826ce
Nuvm <nuvmyt@gmail.com>
2015-10-07 21:47:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var version = '0.1.1 Minor 3';
+var version = '0.2 | Sexiness Update';
 var startUpMsg = 'Welcome to NCS version ' + version + '!';
-var newFeaturesMsg = 'The NCS Website is now online! <a href="https://electricgaming.ga/ncs/" target="_blank">Click here to go to it!</a> <b><i>BIG CHANGES SOON</b></i>(™) :)';
+var newFeaturesMsg = "<a href='https://electricgaming.ga/ncs/' target='_blank'>The NCS Website is now online!</a><br><b><i>MAJOR SMEXINESS INBOUND</b></i>(™)<br>Ok it's definitely not that great but still... not too bad, right?";
 var uname;
+var lastSelected;
 
 //Default vars
 var prevObj = 0;
@@ -36,11 +37,7 @@ function start(s){
 }
 
 //Check for new messages
-/*function scfnm(){
-  setInterval(function(){cfnm();},10);
-}*/
 function cfnm(){
-  //$('#messages')[0].scrollIntoView(false);
   if(document.getElementById('messages').children.length!==prevObj){
     i++;
     prevObj = document.getElementById('messages').children.length;
@@ -146,25 +143,31 @@ if (Notification.permission !== "granted"){
 }
 /*NCS MENU STUFF*/
 function NCSinit(){
-  $('.navbar.footer').append('<div id="NCS-btn" class="animated nav-form nav-right" style="transform:rotate(0deg);background-image:none;bottom:13px;height:30px;width:30px;-webkit-user-select: none;"></div>');
+  $('#chat-button').parent().append('<div id="NCS-btn" class="animated" style="transform:rotate(0deg);background-image:none;height:30px;width:30px;float:right;margin-right:5px;-webkit-user-select: none;"></div>');
   $('#NCS-btn').append('<div id="NCS-name" style="position:absolute;transform:rotate(0deg);font-family:IM Fell English SC;color:black;bottom:10px;text-shadow:0px 0px 2px #FFD700;"><b>NCS</b></div>');
   $('.navbar.header').append('<a href="http://electricgaming.ga/forums/en/forumdisplay.php?fid=24" target="_blank"><button id="THEME_BUG" class="nav-form nav-right">[NCS] Report an Issue</button></a>');
   $('.navbar.header').append('<a href="http://electricgaming.ga/forums/en/showthread.php?tid=5" target="_blank"><button id="THEME_BUG" class="nav-form nav-right">[NCS] Changelog</button></a>');
   $('#NCS-btn')[0].style.backgroundImage = "url('http://i.imgur.com/5ThdRUd.png')";
+  initialWidth1 = $('#chat-button').css('width').split('px')[0];
+  initialWidth2 = $('#users-button').css('width').split('px')[0];
+  initialWidth3 = $('#waitlist-button').css('width').split('px')[0];
+  $('#chat-button').parent().css('height','35px');
+  $('#chat').css('height',($('#chat').css('height').split('px')[0]-5));
+  $('#chat-button').css('width',initialWidth1*0.9);
+  $('#users-button').css('width',initialWidth2*0.9).css('left',$('#users-button').css('left').split('px')[0]-((initialWidth2)-(initialWidth2*0.9)));
+  $('#waitlist-button').css('width',initialWidth3*0.9).css('left',$('#waitlist-button').css('left').split('px')[0]-(((initialWidth3)-(initialWidth3*0.9))+((initialWidth2)-(initialWidth2*0.9))));
+  $('#NCS-btn').css('top','0px').css('right','0px');
+  $('#app-right').append('<div id="NCS-menu" style="display:none;opacity:0.4;background-color:whitesmoke;box-shadow: inset 0px 0px 10px 4px rgba(0,0,0,0.9);"></div>');
+  $('#NCS-menu').css('top','100%-35').css('height',$('#chat').css('height')).css('width','100%');
   //setInterval(function(){rotateDeg+=2;rotateDeg2-=2;$('#NCS-btn')[0].style.transform='rotate('+rotateDeg+'deg)';$('#NCS-name')[0].style.transform='rotate('+rotateDeg2+'deg)';},20);
-  //$('body').append('<div id="NCS-menu" class="animated" style="display:none;position:absolute;bottom:52px;left:-50px;background-color:#0a0a0a;height:140px;width:280px;color:gray;border:2px #1B1B1B solid;text-align:left;z-index:3;opacity:0.8"><!--<center class="animated infinite flip" style="text-align:center">Whoa, Animations!</center>--></div>');
-  $('#NCS-menu').append('<div id="NCS-f1" class="disabled animated" style="float:left;margin:5px;word-wrap:break-word;opacity:0.8">Hide Video Player</div>');
-  $('#NCS-menu').append('<div id="NCS-f2" class="disabled animated" style="float:left;margin:5px;word-wrap:break-word;opacity:0.8">Custom Theme</div>');
-  $('#NCS-menu').append('<div id="NCS-f3" class="disabled animated" style="float:left;margin:5px;word-wrap:break-word;opacity:0.8">Desktop Notifications</div>');
-  $('#NCS-menu').append('<div id="NCS-f4" class="disabled animated" style="float:left;margin:5px;word-wrap:break-word;opacity:0.8">Remove Video Player</div>');
-  $('#NCS-menu').css('left',(($('#NCS-btn').css('width').split('px')[0]/2)-$('#NCS-btn').offset().left)*-1+'px');
-  $('#NCS-btn').on('click',function(){$('#NCS-menu').css('left',(($('#NCS-btn').css('width').split('px')[0]/2)-$('#NCS-btn').offset().left)*-1+'px');if($('#NCS-menu').css('display')==='block'){$('#NCS-menu').addClass('fadeOutRight');$('#NCS-menu').removeClass('fadeInLeft');setTimeout(function(){$('#NCS-menu').css('display','none');$('#NCS-menu').css('left',(($('#NCS-btn').css('width').split('px')[0]/2)-$('#NCS-btn').offset().left)*-1+'px');},500);}else{$('#NCS-menu').addClass('fadeInLeft');$('#NCS-menu').css('display','block');$('#NCS-menu').removeClass('fadeOutRight');}});
-  //window.onresize = function(){$('#NCS-menu').css('left',(($('#NCS-btn').css('width').split('px')[0]/2)-$('#NCS-btn').offset().left*-1)+'px');};
-  $('#NCS-f1').on('click',NCSfeatures);
-  $('#NCS-f2').on('click',NCSfeatures);
-  $('#NCS-f3').on('click',NCSfeatures);
-  $('#NCS-f4').on('click',NCSfeatures);
-  $('head').append('<style type="text/css">.enabled{background-color:#B4CFEC;color:white; !important}.disabled{color:black; !important}#NCS-menu{padding:3px}#NCS-btn:hover{cursor:pointer;background-color:grey;}.NCS-checkmark{float:right;background-image:url("http://i.imgur.com/VzMmfJJ.png?1");}</style>');
+  $('#NCS-menu').append('<div id="NCS-f1" class="disabled animated NCSf">Hide Video Player<span id="NCS-f1c" class="NCS-checkmark" style="display:none"/></div>');
+  $('#NCS-menu').append('<div id="NCS-f2" class="disabled animated NCSf" style="top:34px;">Custom Theme<span id="NCS-f2c" class="NCS-checkmark" style="display:none"/></div>');
+  $('#NCS-menu').append('<div id="NCS-f3" class="disabled animated NCSf" style="top:68px;">Desktop Notifications<span id="NCS-f3c" class="NCS-checkmark" style="display:none"/></div>');
+  $('#NCS-menu').append('<div id="NCS-f4" class="disabled animated NCSf" style="top:102px;">Remove Video Player<span id="NCS-f4c" class="NCS-checkmark" style="display:none"/></div>');
+  $('#NCS-btn').on('click',function(){if($('#NCS-menu').css('display')==='block'){$('#'+lastSelected.split('-button')[0]).css('display','block');$('#NCS-menu').css('display','none');}else{if(document.getElementsByClassName('selected')[0]){lastSelected = document.getElementsByClassName('selected')[0].id;}$('#chat-button,#users-button,#waitlist-button').removeClass('selected');$('#chat,#users,#waitlist').css('display','none');$('#NCS-menu').css('display','block');}});
+  $('#chat-button,#users-button,#waitlist-button').on('click',function(){if($('#NCS-menu').css('display')==='block'){$('#NCS-menu').css('display','none');}});
+  $('#NCS-f1,#NCS-f2,#NCS-f3,#NCS-f4').on('click',NCSfeatures);
+  $('head').append('<style type="text/css">#NCS-btn:hover{cursor:pointer;background-color:grey;}.NCS-checkmark{float:right;background-image:url("http://i.imgur.com/VzMmfJJ.png?1");background-repeat:no-repeat;height:15px;width:15px;margin-right:25px;}.NCSf{height:15px;word-wrap:break-word;opacity:0.8;padding-top:13px;padding-bottom:13px;padding-left:15px;color:black;}.NCSf:hover{cursor:pointer;box-shadow:inset 0px 0px 9px 1px rgba(139,0,139,0.8);}</style>');
   $('#messages').append('<center id="NCS-startupmsg" class="cm log mention animated flip" style="color:whitesmoke;text-align:center;font-weight:200;font-size:120%;padding:30px;">'+startUpMsg+'<br><span style="font-weight:100;font-size:85%">'+newFeaturesMsg+'</span></center>');
   $('#messages')[0].scrollIntoView(false);
   if(NCSsettings[0]){
@@ -183,52 +186,56 @@ function NCSinit(){
 function NCSfeatures(eventData){
   if(eventData.target.id==='NCS-f1'){
     if($('#NCS-f1').hasClass('disabled')){
-      document.getElementById('player').style.display = 'none';
-      NCSsettings[0] = true;
-      $('#NCS-f1').removeClass('disabled').addClass('enabled');
-      //$('#NCS-f1').removeClass('shake').addClass('bounce');
+      if(document.getElementById('player')){
+        $('#NCS-f1c').css('display','block');
+        document.getElementById('player').style.display = 'none';
+        NCSsettings[0] = true;
+        $('#NCS-f1').removeClass('disabled').addClass('enabled');
+      }
     } else {
-      document.getElementById('player').style.display = 'block';
-      NCSsettings[0] = false;
-      $('#NCS-f1').removeClass('enabled').addClass('disabled');
-      //$('#NCS-f1').removeClass('bounce').addClass('shake');
+      if(document.getElementById('player')){
+        $('#NCS-f1c').css('display','none');
+        document.getElementById('player').style.display = 'block';
+        NCSsettings[0] = false;
+        $('#NCS-f1').removeClass('enabled').addClass('disabled');
+      }
     }
   } else if(eventData.target.id==='NCS-f2'){
     if($('#NCS-f2').hasClass('disabled')){
       $('head').append('<link id="CSxKINGtheme" rel="stylesheet" href="https://cdn.rawgit.com/Nuvm/NCS/dev/NC331_Style.css">');
+      $('#NCS-f2c').css('display','block');
       NCSsettings[1] = true;
       $('#NCS-f2').removeClass('disabled').addClass('enabled');
-      //$('#NCS-f2').removeClass('shake').addClass('bounce');
     } else {
+      $('#NCS-f2c').css('display','none');
       $('#CSxKINGtheme').remove();
       NCSsettings[1] = false;
       $('#NCS-f2').removeClass('enabled').addClass('disabled');
-      //$('#NCS-f2').removeClass('bounce').addClass('shake');
     }
   } else if(eventData.target.id==='NCS-f3'){
     if($('#NCS-f3').hasClass('disabled')){
+      $('#NCS-f3c').css('display','block');
       NCSsettings[2] = true;
       $('#NCS-f3').removeClass('disabled').addClass('enabled');
-      //$('#NCS-f3').removeClass('shake').addClass('bounce');
     } else {
+      $('#NCS-f3c').css('display','none');
       NCSsettings[2] = false;
       $('#NCS-f3').removeClass('enabled').addClass('disabled');
-      //$('#NCS-f3').removeClass('bounce').addClass('shake');
     }
   } else if(eventData.target.id==='NCS-f4'){
     if($('#NCS-f4').hasClass('disabled')){
+      $('#NCS-f4c').css('display','block');
       $('#player').remove();
       $('#messages').append('<center class="NCSalert cm log mention animated fadeInLeftBig" style="color:whitesmoke;text-align:center;font-weight:150;font-size:30;">You will have to disable this option and refresh in order to have the video player back. Sorry, this is how it works currently!</center>');
       $('#messages')[0].scrollIntoView(false);
       NCSsettings[3] = true;
       $('#NCS-f4').removeClass('disabled').addClass('enabled');
-      //$('#NCS-f3').removeClass('shake').addClass('bounce');
     } else {
+      $('#NCS-f4c').css('display','none');
       $('#messages').append('<center class="NCSalert cm log mention animated fadeInLeftBig" style="color:whitesmoke;text-align:center;font-weight:150;font-size:30;">Refresh the page to get the video player back!</center>');
       $('#messages')[0].scrollIntoView(false);
       NCSsettings[3] = false;
       $('#NCS-f4').removeClass('enabled').addClass('disabled');
-      //$('#NCS-f3').removeClass('bounce').addClass('shake');
     }
   }
 }
@@ -246,15 +253,3 @@ function confirmExit() {
     }
 }
 */
-initialWidth1 = $('#chat-button').css('width').split('px')[0];
-initialWidth2 = $('#users-button').css('width').split('px')[0];
-initialWidth3 = $('#waitlist-button').css('width').split('px')[0];
-$('#chat-button').parent().css('height','50px');
-$('#chat').css('height',$('#chat').css('height').split('px')[0]-20);
-$('#chat-button').css('width',initialWidth1*0.85);
-$('#users-button').css('width',initialWidth2*0.85).css('left',$('#users-button').css('left').split('px')[0]-((initialWidth2)-(initialWidth2*0.85)));
-$('#waitlist-button').css('width',initialWidth3*0.85).css('left',$('#waitlist-button').css('left').split('px')[0]-(((initialWidth3)-(initialWidth3*0.85))+((initialWidth2)-(initialWidth2*0.85))));
-$('#NCS-btn').css('top','51px').css('right','0px');
-$('#chat-button').parent().append('<div id="#NCS-menu" style="display:none;opacity:0.6px;></div>');
-$('#NCS-menu').css('top','101px').css('right','0px').css('height',100%-152).css('width',$('#chat').css('width',$('#chat').css('width').split('px')[0]));
-
