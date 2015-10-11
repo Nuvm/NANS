c2653cb38ed1a029012556a789244aa104bfad09
Nuvm <nuvmyt@gmail.com>
2015-10-10 20:28:34 -0400

    Update CustomNames.js

--- CustomNames.js
+++ CustomNames.js
@@ -5,31 +5,31 @@ function cfun(){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
       unameicon = document.getElementById('messages').lastChild.getElementsByClassName('icon-rank-2');
-      $(unameicon).before('<i id='+("icon-01NCS"+i)+' class="icon" style="background-image:none"></i>');
-      document.getElementById('icon-01NCS'+i).style.backgroundImage = "url('http://imgur.com/3hN3fNi.png')";
+      $(unameicon).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
+      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://imgur.com/3hN3fNi.png')";
       $(unamestuff).removeClass('rank-2');
       unamestuff.style.color='#A1BA00';
     }
     if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='Loli'){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
-      $(unamestuff).before('<i id='+("icon-02NCS"+i)+' class="icon" style="background-image:none"></i>');
-      document.getElementById('icon-02NCS'+i).style.backgroundImage = "url('http://i.imgur.com/BSky1IS.png')";
+      $(unamestuff).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
+      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://i.imgur.com/BSky1IS.png')";
       unamestuff.style.color='#9966FF';
     }
     if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='Pro Hop'){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
       unameicon = document.getElementById('messages').lastChild.getElementsByClassName('icon-rank-2');
-      $(unameicon).before('<i id='+("icon-03NCS"+i)+' class="icon" style="background-image:none"></i>');
-      document.getElementById('icon-03NCS'+i).style.backgroundImage = "url('http://imgur.com/f5HTF9D.png')";
+      $(unameicon).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
+      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://imgur.com/f5HTF9D.png')";
       $(unamestuff).removeClass('rank-2');
       unamestuff.style.color='#B20DA5';
     }
     if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='Elitehunter47'){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
-      $(unamestuff).before('<i id='+("icon-04NCS"+i)+' class="icon" style="background-image:none"></i>');
+      $(unamestuff).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
       document.getElementById('icon-04NCS'+i).style.backgroundImage = "url('http://i.imgur.com/Wyh8Mbv.png')";
       unamestuff.style.color='#EA6900';
     }
@@ -37,8 +37,8 @@ function cfun(){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
       unameicon = document.getElementById('messages').lastChild.getElementsByClassName('icon-rank-3');
-      $(unameicon).before('<i id='+("icon-00NCS"+i)+' class="icon icon-rank-10"></i>');
-      //document.getElementById('icon-04NCS'+i).style.backgroundImage = "url('http://i.imgur.com/Wyh8Mbv.png')";
+      $(unameicon).before('<i id='+("icon-NCS"+i)+' class="icon icon-rank-10"></i>');
+      //document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://i.imgur.com/Wyh8Mbv.png')";
       //unamestuff.style.color='#EA6900';
       $(unamestuff).removeClass('rank-3');
       $(unamestuff).addClass('rank-10');
@@ -46,16 +46,16 @@ function cfun(){
     if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='Koalaaa__'){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
-      $(unamestuff).before('<i id='+("icon-05NCS"+i)+' class="icon" style="background-image:none"></i>');
-      document.getElementById('icon-05NCS'+i).style.backgroundImage = "url('http://i.imgur.com/ucqMWad.jpg')";
+      $(unamestuff).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
+      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://i.imgur.com/ucqMWad.jpg')";
       //unamestuff.style.color='#EA6900';
     }
     if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='KuriaMisuto'){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
       unameicon = document.getElementById('messages').lastChild.getElementsByClassName('icon-rank-2');
-      $(unameicon).before('<i id='+("icon-06NCS"+i)+' class="icon" style="background-image:none"></i>');
-      document.getElementById('icon-06NCS'+i).style.backgroundImage = "url('http://i.imgur.com/LXoYihi.png')";
+      $(unameicon).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
+      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://i.imgur.com/LXoYihi.png')";
       $(unamestuff).removeClass('rank-2');
       unamestuff.style.color='#00E5EE';
     }
@@ -63,8 +63,8 @@ function cfun(){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
       unameicon = document.getElementById('messages').lastChild.getElementsByClassName('icon-rank-2');
-      $(unameicon).before('<i id='+("icon-07NCS"+i)+' class="icon" style="background-image:none"></i>');
-      document.getElementById('icon-07NCS'+i).style.backgroundImage = "url('http://i.imgur.com/1kemcRT.pngQ')";
+      $(unameicon).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
+      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://i.imgur.com/1kemcRT.pngQ')";
       //$(unamestuff).removeClass('rank-2');
       //unamestuff.style.color='#00E5EE';
     }
@@ -72,9 +72,18 @@ function cfun(){
       i++;
       unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
       unameicon = document.getElementById('messages').lastChild.getElementsByClassName('icon-rank-2');
-      $(unameicon).before('<i id='+("icon-07NCS"+i)+' class="icon" style="background-image:none"></i>');
-      document.getElementById('icon-07NCS'+i).style.backgroundImage = "url('http://i.imgur.com/Gcf5bSn.png')";
+      $(unameicon).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
+      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://i.imgur.com/Gcf5bSn.png')";
       $(unamestuff).removeClass('rank-2');
       unamestuff.style.color='#FF29EE';
+    }
+    if(document.getElementById('messages').lastChild.getElementsByClassName('uname')[0].innerHTML==='Danklight'){
+      i++;
+      unamestuff = document.getElementById('messages').lastChild.getElementsByClassName('uname')[0];
+      unameicon = document.getElementById('messages').lastChild.getElementsByClassName('icon-rank-2');
+      $(unameicon).before('<i id='+("icon-NCS"+i)+' class="icon" style="background-image:none"></i>');
+      document.getElementById('icon-NCS'+i).style.backgroundImage = "url('http://imgur.com/GT0yJLY.png')";
+      $(unamestuff).removeClass('rank-2');
+      unamestuff.style.color='#FF0000';
     }
 }
