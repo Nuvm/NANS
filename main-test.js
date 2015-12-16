d59dcad07c3363b200379eda1aebb67cd95b85b3
WindWalk12 <nhygta@hotmail.com>
2015-12-16 22:17:33 +0100

    Half it working now, so should the last part.

--- main-test.js
+++ main-test.js
@@ -431,6 +431,7 @@ function NCSfeatures(eventData) {
     $('#NCS-f12c').css('display', 'none');
     NCSsettings[12] = false;
     $('#NCS-f12').removeClass('enabled').addClass('disabled');
+    ETA();
    }
   }
 }
