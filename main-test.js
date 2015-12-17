d19c6bb813fb962774f7fa9acf1f90a3be667559
WindWalk12 <nhygta@hotmail.com>
2015-12-17 11:43:15 +0100

    Update main-test.js

--- main-test.js
+++ main-test.js
@@ -550,6 +550,7 @@ if (NCSsettings[12] == true) {
 (function() {
   $('head').prepend('<style>#waitlist-join::after { content: attr(data-eta); display: block; }</style>');
   $('head').prepend('<style>#waitlist-join::after { padding: 0px !important; }</style>');
+  $('head').append('<style>#waitlist-join::after { display: block; }</style>');
   // $('head').prepend('<style>input[type="text"] { position: relative; bottom: 11px; right: 0px; width: 230px; height: 11px; }</style>');
 
   function readable(total) {
@@ -586,7 +587,7 @@ var ETAInterval = setInterval(function() {
 };
 function ETAOff() {
  if (NCSsettings[12] == false) {
-  $('head').prepend('<style>#waitlist-join::after { display: none; }</style>');
-  $('head').prepend('<style>#waitlist-join::after { padding: 7px !important; }</style>');
+  $('head').append('<style>#waitlist-join::after { display: none; }</style>');
+  $('head').append('<style>#waitlist-join { padding: 7px !important; }</style>');
   }
 };
